1956671859f714a1ec34a35aa41c64756b2a70bd
*vivek <vrvivekofficial@gmail.com>
2025-01-10 14:54:51 +0530

    create route and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Loader from "./components/Loader";
+
+const Home = lazy(() => import("./pages/HomePage"));
+const Search = lazy(() => import("./pages/SearchPage"));
+const Cart = lazy(() => import("./pages/CartPage"));
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 };
 
 export default App;
