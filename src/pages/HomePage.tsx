65fa51906b9038f079b9d4695637e92c239d440e
*vivek <vrvivekofficial@gmail.com>
2025-01-11 16:11:02 +0530

    create cart component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,30 @@
-
 import { Link } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
+  const addTocartHandler: any = () => {};
+
+
   return (
     <div className="home">
-      <section></section>
+      <section>
+        
+      </section>
       <h1>
         Latest Products
-        <Link to={"/search"} className="findmore">More</Link>
+        <Link to={"/search"} className="findmore">
+          More
+        </Link>
       </h1>
       <main>
-
+        <ProductCard
+          name="macbook"
+          photo="https://m.media-amazon.com/images/I/71jG+e7roXL._SX679_.jpg"
+          productId="aaaaa"
+          price={900}
+          stock={99}
+          handler={addTocartHandler}
+        />
       </main>
     </div>
   );
