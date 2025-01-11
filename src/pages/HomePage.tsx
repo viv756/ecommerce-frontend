0ae07021c0436dacb0d090854d6fad931f852539
*vivek <vrvivekofficial@gmail.com>
2025-01-11 14:06:12 +0530

    create banner section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products
+        <Link to={"/search"} className="findmore">More</Link>
+      </h1>
+      <main>
+
+      </main>
+    </div>
+  );
 };
 
 export default HomePage;
